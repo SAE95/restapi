43d203b1ade99442dfa8b353e162eb3f8b38cafd
SAE95 <design.agx@gmail.com>
2022-05-20 17:31:13 +0200

    First Commit

---
+++ pages/api/trips/index.js
@@ -0,0 +1,12 @@
+import prisma from 'lib/prisma'
+
+export default function handler(req, res) {
+    if (req.method !== 'GET') {
+      res.status(405).json({ 
+              message: 'Method Not Allowed' 
+          })
+      return
+    }
+  
+    res.status(200).json({ test: 'TRIPS!' })
+  }
