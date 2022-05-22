dc3b9d40e35a3224b0a50ebe9f4152ae6a6710d6
SAE95 <design.agx@gmail.com>
2022-05-22 16:54:55 +0200

    added expenses post

--- pages/api/expenses/index.js
+++ pages/api/expenses/index.js
@@ -11,6 +11,41 @@ export default async function handler(req, res) {
   }
 
   if (req.method === 'POST') {
+      const { trip, name, date, amount, currency } = req.body
+
+      if (!trip) {
+          return res
+          .status(400)
+          .json({message: 'missing required parameters'})
+      }
+
+      if (!name) {
+        return res
+        .status(400)
+        .json({message: 'missing required parameters'})
+    }
+
+    if (!amount) {
+        return res
+        .status(400)
+        .json({message: 'missing required parameters'})
+    }
+
+    if (!currency) {
+        return res
+        .status(400)
+        .json({message: 'missing required parameters'})
+    }
+
+    await prisma.expense.create({
+        data: {
+            trip,
+            name,
+            date,
+            amount,
+            currency,
+        },
+    })
 
 	return	res.status(200).end()
   }
