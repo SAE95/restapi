e5eac43ac0d33221de7fb5dc867e142bee15f062
SAE95 <design.agx@gmail.com>
2022-05-22 17:08:02 +0200

    add put endpoint

--- pages/api/trips/index.js
+++ pages/api/trips/index.js
@@ -4,9 +4,18 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
 
       const trips = await prisma.trip.findMany()
-      res.status(200).json(trips)
 
-      return
+      await Promise.all(
+        trips.map(async(trip)=> {
+          trip.expenses = await prisma.expense.findMany({
+            where: {
+              trip: trip.id,
+            },
+          })
+        })
+      )
+      return res.status(200).json(trips)
+
     
   }
 
