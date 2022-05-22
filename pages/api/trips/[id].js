05fdd0bbee351c87dbd8f474f631f447cd1c183c
SAE95 <design.agx@gmail.com>
2022-05-22 16:02:31 +0200

    enpoints2

--- pages/api/trips/[id].js
+++ pages/api/trips/[id].js
@@ -3,6 +3,11 @@ import prisma from 'lib/prisma'
 export default async function handler(req, res) {
   
     if (req.method === 'GET') {
+        const trip = await prisma.trip.findUnique({
+            where: {
+                id: parseInt(req.query.id)
+            },
+        })
 
     }
     if (req.method === 'PUT') {
